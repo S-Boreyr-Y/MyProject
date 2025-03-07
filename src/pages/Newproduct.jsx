724752010c391eb7b2ccffb1ea97ba9e0444714c
s.boreyr.y <s.boreyr.y@gmail.com>
2025-03-07 05:12:31 +0330

    14031216-5:12-post axios

--- src/pages/Newproduct.jsx
+++ src/pages/Newproduct.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import stylenewproduct from "../styles/Newproduct/Newproduct.module.scss";
 import store from "../components/common/Productheader/images/store-01.svg";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Newproduct() {
+  const [product, setProduct] = useState({
+    title:"aaa",
+    details:"bbbb"
+  });
+
+  const postaxios =  () => {
+     axios.post("https://67c56fef351c081993f9d9ff.mockapi.io//product",
+      {
+        title:product.title,
+        details:product.details
+      });
+  };
+
+  const post=()=>{
+    postaxios();
+console.log(product)
+  }
+
+
+
+
+
+
   return (
     <div>
       <div className={stylenewproduct.newproduct}>
@@ -30,7 +54,7 @@ function Newproduct() {
             <span className={stylenewproduct.date}>Date</span>
             <input type="text" name="" id="" />
           </div>
-        <Link className={stylenewproduct.btn}>Add product</Link>
+          <Link className={stylenewproduct.btn} onClick={post}>Add product</Link>
         </div>
       </div>
     </div>
